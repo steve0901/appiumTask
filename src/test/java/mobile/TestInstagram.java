4693e1c98cc009ac3e7fb745f734bf1631143040
Synoviat <stepan.synoviat@gmail.com>
2019-07-04 08:01:40 +0300

    added logger

--- src/test/java/mobile/TestInstagram.java
+++ src/test/java/mobile/TestInstagram.java
@@ -4,26 +4,33 @@ import mobile.driverManager.DriverManager;
 import mobile.pageObject.FollowersPO;
 import mobile.pageObject.LoginPO;
 import mobile.pageObject.MainMenuPO;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.After;
 import org.junit.Test;
 
 public class TestInstagram {
+	private Logger logger = LogManager.getLogger(TestInstagram.class);
 
 	@Test
 	public void enterInstagramProfile() {
-		LoginPO loginPO = new LoginPO();
-		loginPO.enterInstagram();
 		String userName = "myFBprofile";
 		String password = "myFBpassword";
+		String follower = "ukrainer";
 
+		LoginPO loginPO = new LoginPO();
+		logger.info("open instagram ");
+		loginPO.enterInstagram();
+		logger.info("enter login and password ");
 		loginPO.enterProfileData(userName, password);
 
 		MainMenuPO mainMenuPO = new MainMenuPO();
+		logger.info("find some following user");
 		mainMenuPO.openFollowingUsers();
 
 		FollowersPO followersPO = new FollowersPO();
-		String follower = "ukrainer";
 		followersPO.findSomeFollowingUser(follower);
+		logger.info("user was found");
 	}
 
 	@After
